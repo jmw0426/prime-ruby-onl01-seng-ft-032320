36542612af72e361c0d886cfb2f01272caac9b80
Joshua Williams <jmw0426@hotmail.com>
2020-03-27 22:55:27 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,18 @@
-def prime? n
-  for num in 2..(n - 1)
-   if (n % num) == 0
-    return false
-   end
-  end
-   true
+# def prime? n
+#   for num in 2..(n - 1)
+#   if (n % num) == 0
+#     return false
+#   end
+#   end
+#   true
   
- end
+# end
   
+def prime?(n)
+  return false if n < 2
+
+  (2..n/2).none?{|i| n % i == 0}
+end
   
   
     
