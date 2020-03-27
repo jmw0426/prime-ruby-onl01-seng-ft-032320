b2c4945626da1141364abb6ebdc3f1e3524bb343
Joshua Williams <jmw0426@hotmail.com>
2020-03-27 23:04:01 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,9 @@
-def prime?(n) #inputted integer (ii)
-  return false if n < 2 #return false if (ii) is 
+def prime?(n) #(n) = inputted integer (ii)
+  return false if n < 2 
+  #Returns false if (ii) is less than 2 so that negative numbers and 1 are excluded.
 
-  (2..n/2).none?{|x| n % x == 0}
+  (2..n/2).none?{|x| n % x == 0} 
+  #The range starts at 2 and ends at (ii) divided by 2. none? returns true if no elements are true. The object |x| is equal to (ii) divided by |x| compared to 0 to see if it is constantly divisible.  
 end
   
   
